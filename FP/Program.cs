34bdc667016227891e27ec39797ba8b8a10d4512
Tepes Alexandru <godzplayer@hotmail.com>
2020-10-16 11:50:37 +0300

    problema cu while

--- FP/Program.cs
+++ FP/Program.cs
@@ -4,9 +4,38 @@ namespace FP
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a, b, c, d, e;
+            a = rnd.Next(2, 13);
+            b = rnd.Next(2, 13);
+            c = rnd.Next(2, 13);
+            d = rnd.Next(2, 13);
+            e = rnd.Next(2, 13);
+
+            Console.WriteLine($"{a} {b} {c} {d} {e}");
+
+            // vector de frecventa
+            int[] v = new int[13];
+            for (int i = 0; i < 13; i++)
+            {
+                v[i] = 0;
+            }
+
+            v[a]++;
+            v[b]++;
+            v[c]++;
+            v[d]++;
+            v[e]++;
+
+            for (int i = 0; i < 13; i++)
+            {
+                Console.Write($"{v[i]} ");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Rand/Rand/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+
+namespace Rand
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            // incercam o metoda aleatoare
+            int[] v = new int[] { 1, 6, 3, 1, 1, 2, 5, 1, 1, 4, 2 };
+            int apa = 0;
+            for (int j = 0; j < v.Length; j++)
+            {
+                bool ok = false;
+                do
+                {
+                    ok = false;
+                    int p = j;
+                    bool isLeft = false;
+                    bool isRight = false;
+                    for (int i = p - 1; i >= 0; i--)
+                    {
+                        if (v[i] > v[p])
+                        {
+                            isLeft = true;
+                            break;
+                        }
+                    }
+                    for (int i = p + 1; i < v.Length; i++)
+                    {
+                        if (v[i] > v[p])
+                        {
+                            isRight = true;
+                            break;
+                        }
+                    }
+                    if (isLeft && isRight)
+                    {
+                        apa++;
+                        v[p]++;
+                        ok = true;
+                    }
+                } while (ok);
+                
+            }
+            Console.WriteLine(apa);
+            Console.ReadKey();
+        }
+    }
+}
