f04bce352e548347dc02acc3a70a3ac03dc11667
Tepes Alexandru <godzplayer@hotmail.com>
2020-10-31 23:40:44 +0200

    Forms - Linie Minima : Comentarii

--- LinieMinima/LinieMinima/Form1.cs
+++ LinieMinima/LinieMinima/Form1.cs
@@ -41,26 +41,35 @@ namespace LinieMinima
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // legam codul nostru de imaginea care se afiseaza pe ecran
             bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             grp = Graphics.FromImage(bmp);
+
+            // setam culoarea de fundal ca fiind neagra
             grp.Clear(Color.Black);
 
+            // declaram cele n puncte
             int n = 100;
             point[] V = new point[n];
 
+            // generam cele n puncte
             for (int i = 0; i < n; i++)
             {
                 V[i] = new point();
             }
+
+            // le desenam pe ecran
             for (int i = 0; i < n; i++)
             {
                 V[i].Draw(grp);
             }
 
+            // aflam distanta minima de la punct la punct
             for (int j = 0; j < n - 1; j++)
             {
                 float lungmin = lungime(V[j], V[j + 1]);
                 int idx = j + 1;
+                // calculam lungimea de la punctul actual la toate celelalte puncte
                 for (int i1 = j + 1; i1 < n; i1++)
                 {
                     float l = lungime(V[j], V[i1]);
@@ -70,12 +79,17 @@ namespace LinieMinima
                         idx = i1;
                     }
                 }
+                
+                // dupa ce o gasim pe cea minima, o punem pe pozitia urmatoare din vector
                 point aux = V[j + 1];
                 V[j + 1] = V[idx];
                 V[idx] = aux;
+
+                // desenam o linie intre punctul actual si cel mai apropiat punct de el
                 grp.DrawLine(Pens.Gold, V[j].x, V[j].y, V[j + 1].x, V[j + 1].y);
             }
 
+            // actualizam imaginea de pe ecran
             pictureBox1.Image = bmp;
         }
     }
